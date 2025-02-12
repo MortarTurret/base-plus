47c696b22566b08ed7ec2f95c5538a779d834f39
Mortar Turret <bigmikelilienthal+1@gmail.com>
2025-02-12 10:55:10 -0500

    feat: flag-carriers can now pilot and ride in vehicles

--- items/vehicles/vehicle.cs
+++ items/vehicles/vehicle.cs
@@ -1,8 +1,8 @@
 function Vehicle::onAdd(%this)
 {
-	%this.shieldStrength = 0.0;
-	GameBase::setRechargeRate (%this, 10);
-	GameBase::setMapName (%this, "Vehicle");
+  %this.shieldStrength = 0.0;
+  GameBase::setRechargeRate (%this, 10);
+  GameBase::setMapName (%this, "Vehicle");
 }
 
 function Vehicle::onCollision (%this, %object) {
@@ -13,37 +13,39 @@ function Vehicle::onCollision (%this, %object) {
 
 			%armor = Player::getArmor(%object);
 			%client = Player::getClient(%object);
-			if ((%armor == "larmor" || %armor == "lfemale") && Vehicle::canMount (%this, %object)) {
-				if ( %object.carryFlag != "" ) {
-					Client::sendMessage( Player::getClient(%object), 0, "You cannot enter a vehicle with the flag.~wError_Message.wav" );
-					return;
-				}
 
+			if ((%armor == "larmor" || %armor == "lfemale") && Vehicle::canMount (%this, %object)) {
 				%weapon = Player::getMountedItem(%object,$WeaponSlot);
+
 				if(%weapon != -1) {
 					%object.lastWeapon = %weapon;
 					Player::unMountItem(%object,$WeaponSlot);
 				}
+
 				Player::setMountObject(%object, %this, 1);
 				Client::setControlObject(%client, %this);
+
 				playSound (GameBase::getDataName(%this).mountSound, GameBase::getPosition(%this));
+
 				%object.driver= 1;
 				%object.vehicle = %this;
 				%this.clLastMount = %client;
-			} else if(GameBase::getDataName(%this) != Scout) {
-				if ( %object.carryFlag != "" ) {
-					Client::sendMessage( Player::getClient(%object), 0, "You cannot enter a vehicle with the flag.~wError_Message.wav" );
-					return;
-				}
-
+			}
+      
+      else if(GameBase::getDataName(%this) != Scout) {
 				%mountSlot= Vehicle::findEmptySeat(%this,%client); 
+
 				if(%mountSlot) {
 					%object.vehicleSlot = %mountSlot;
 					%object.vehicle = %this;
+
 					Player::setMountObject(%object, %this, %mountSlot);
+
 					playSound (GameBase::getDataName(%this).mountSound, GameBase::getPosition(%this));
 				}
-			} else if (GameBase::getControlClient(%this) == -1) {
+			}
+      
+      else if (GameBase::getControlClient(%this) == -1) {
 				Client::sendMessage(Player::getClient(%object),0,"You must be in Light Armor to pilot the vehicles.~wError_Message.wav");
 			}
 		}
@@ -97,19 +99,20 @@ function getClosestPosition(%num,%playerPos,%slotPos0,%slotPos1,%slotPos2,%slotP
 	return %closePos;
 }
 
-function Vehicle::passengerJump(%this,%passenger,%mom)
-{
+function Vehicle::passengerJump(%this,%passenger,%mom) {
 	%armor = Player::getArmor(%passenger);
 	if(%armor == "larmor" || %armor == "lfemale") {
 		%height = 2;
 		%velocity = 70;
 		%zVec = 70;
 	}
+
 	else if(%armor == "marmor" || %armor == "mfemale") {
 		%height = 2;
 		%velocity = 100;
 		%zVec = 100;
 	}
+
 	else if(%armor == "harmor") {
 		%height = 2;
 		%velocity = 140;
@@ -125,64 +128,78 @@ function Vehicle::passengerJump(%this,%passenger,%mom)
 		%client = Player::getClient(%passenger);
 		%this.Seat[%passenger.vehicleSlot-2] = "";
 		%passenger.vehicleSlot = "";
-	   %passenger.vehicle= "";
+    %passenger.vehicle= "";
+
 		Player::setMountObject(%passenger, -1, 0);
+
 		%rotZ = getWord(GameBase::getRotation(%passenger),2);
+
 		GameBase::setRotation(%passenger, "0 0 " @ %rotZ);
 		GameBase::setPosition(%passenger,%posX @ " " @ %posY @ " " @ (%posZ + %height));
+
 		%jumpDir = Vector::getFromRot(GameBase::getRotation(%passenger),%velocity,%zVec);
+
 		Player::applyImpulse(%passenger,%jumpDir);
 	}
-	else
+
+	else {
 		Client::sendMessage(Player::getClient(%passanger),0,"Can not dismount - Obstacle in the way.~wError_Message.wav");
+  }
 }
 
-function Vehicle::jump(%this,%mom)
-{
+function Vehicle::jump(%this,%mom) {
    Vehicle::dismount(%this,%mom);
 }
 
-function Vehicle::dismount(%this,%mom)
-{
+function Vehicle::dismount(%this,%mom) {
    %cl = GameBase::getControlClient(%this);
+
    if(%cl != -1)
    {
       %pl = Client::getOwnedObject(%cl);
+
       if(getObjectType(%pl) == "Player")
       {
-			%time = getIntegerTime(true) >> 5; 
-			
-			// dismount the player
-			%test = GameBase::testPosition(%pl, Vehicle::getMountPoint(%this,0));
-			%pos = Vehicle::getMountPoint(%this,0);
-			%vehiclepos = getboxcenter(%this);
-			%through = getlosinfo(%vehiclepos,vector::add(%pos,"0 0 2"),1);
-			if(%test && !%through) 
-			{
-				%pl.lastMount = %this;
-				%pl.newMountTime = getSimTime() + 3.0;
-				Player::setMountObject(%pl, %this, 0);
-				Player::setMountObject(%pl, -1, 0);
-				%rot = GameBase::getRotation(%this);
-				%rotZ = getWord(%rot,2);
-				GameBase::setRotation(%pl, "0 0 " @ %rotZ);
-				Player::applyImpulse(%pl,%mom);
-	        	 	Client::setControlObject(%cl, %pl);
-				playSound (GameBase::getDataName(%this).dismountSound, GameBase::getPosition(%this));
-				if(%pl.lastWeapon != "") 
-				{
-					Player::useItem(%pl,%pl.lastWeapon);		 	
-					%pl.lastWeapon = "";
-	      			}
-				%pl.driver = "";
-				%pl.vehicle = "";
-			}
-			else if(%time > %cl.lastTryDismount)
-				Client::sendMessage(%cl,0,"Can not dismount - Obstacle in the way.~wError_Message.wav");
-				
-			
-			%cl.lastTryDismount = %time;
-		}
+        %time = getIntegerTime(true) >> 5; 
+
+        // dismount the player
+        %test = GameBase::testPosition(%pl, Vehicle::getMountPoint(%this,0));
+        %pos = Vehicle::getMountPoint(%this,0);
+        %vehiclepos = getboxcenter(%this);
+        %through = getlosinfo(%vehiclepos,vector::add(%pos,"0 0 2"),1);
+
+        if(%test && !%through) {
+          %pl.lastMount = %this;
+          %pl.newMountTime = getSimTime() + 3.0;
+
+          Player::setMountObject(%pl, %this, 0);
+          Player::setMountObject(%pl, -1, 0);
+
+          %rot = GameBase::getRotation(%this);
+          %rotZ = getWord(%rot,2);
+
+          GameBase::setRotation(%pl, "0 0 " @ %rotZ);
+          Player::applyImpulse(%pl,%mom);
+          Client::setControlObject(%cl, %pl);
+
+          playSound (GameBase::getDataName(%this).dismountSound, GameBase::getPosition(%this));
+
+          if(%pl.lastWeapon != "") 
+          {
+            Player::useItem(%pl,%pl.lastWeapon);		 	
+            %pl.lastWeapon = "";
+          }
+
+          %pl.driver = "";
+          %pl.vehicle = "";
+        }
+
+        else if(%time > %cl.lastTryDismount) {
+          Client::sendMessage(%cl,0,"Can not dismount - Obstacle in the way.~wError_Message.wav");
+        }
+
+        %cl.lastTryDismount = %time;
+      }
    }
 }
 
@@ -225,4 +242,14 @@ function Vehicle::getHeatFactor(%this)
 	// Not getting called right now because turrets don't track
 	// vehicles.  A hack has been placed in Player::getHeatFactor.
    return 1.0;
+}
+
+function Vehicle::onPilot(%this, %player)
+{
+
+}
+
+function Vehicle::onUnPilot(%this, %player)
+{
+
 }
